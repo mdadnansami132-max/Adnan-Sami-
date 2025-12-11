537a1b9c34fb25795591c26a9a90d4d70bcf75e3
mdadnansami132-max <mdadnansami132@gmail.com>
2025-12-11 03:45:21 +0300

    Update stopwar.js

--- Script/commands/stopwar.js
+++ Script/commands/stopwar.js
@@ -1,1 +1,29 @@
+module.exports.config = {
+    name: "stopwar",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "CHATGPT",
+    description: "Stop running war",
+    commandCategory: "fun",
+    usages: "",
+    cooldowns: 1
+};
 
+const ownerUID = "100047693744912";
+const activeWars = global.activeWars || (global.activeWars = {});
+
+module.exports.run = function ({ api, event }) {
+
+    if (event.senderID !== ownerUID)
+        return api.sendMessage("এই কমান্ড শুধু মালিক চালাতে পারবে!", event.threadID);
+
+    const threadID = event.threadID;
+
+    if (!activeWars[threadID])
+        return api.sendMessage("বর্তমানে কোন war চলছে না!", threadID);
+
+    clearInterval(activeWars[threadID]);
+    delete activeWars[threadID];
+
+    return api.sendMessage("⛔ War Stopped.", threadID);
+};
