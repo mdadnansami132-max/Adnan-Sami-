b2d82623dd409279e13c0332c967398ccd21a2f0
mdadnansami132-max <mdadnansami132@gmail.com>
2025-12-11 05:03:55 +0300

    Update shahid.js

--- Script/commands/shahid.js
+++ Script/commands/shahid.js
@@ -1,1 +1,25 @@
+module.exports.config = {
+    name: "shahid",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "De Lara Chini Chara",
+    description: "Shahid command reply",
+    commandCategory: "fun",
+    usages: "shahid",
+    cooldowns: 2,
+};
 
+module.exports.run = async function ({ api, event }) {
+    const replyTexts = [
+        "🔥 শাহিদ ভাই আসছেসেন বলো?",
+        "😎 শাহিদ আসলে স্টাইল লেভেল বাড়ে!",
+        "💥 শাহিদ নামটা শুনলেই পাওয়ার বের হয়!",
+        "😈 শাহিদ ভাই রিয়েল বস!",
+        "😂 শাহিদ কি করসেন আবার!"
+    ];
+
+    // Random message select
+    const text = replyTexts[Math.floor(Math.random() * replyTexts.length)];
+
+    return api.sendMessage(text, event.threadID, event.messageID);
+};
